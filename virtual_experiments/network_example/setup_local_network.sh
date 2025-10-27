5d6072b800edda6e15311e183c9b342270687efc
Johannes Hjorth <hjorth@kth.se>
2025-10-27 15:49:16 +0100

    Work in progress

---
+++ virtual_experiments/network_example/setup_local_network.sh
@@ -0,0 +1,4 @@
+python setup_network.py
+snudda create networks/striatum_with_dopamine/ --input data/dopamine_experiment_input.json
+
+# snudda simulate networks/striatum_with_dopamine/ --simulation_config data/dopamine_experiment_simulation.json
